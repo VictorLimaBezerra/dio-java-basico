c72f5a2f03bea83a60b397412c17c13b818662e0
Victor Lima Bezerra <lima.victoradm@gmail.com>
2024-06-08 12:04:00 -0300

    adição de exercicio simples sobre incremento e decremento

---
+++ dio-java-exercicios-1/src/IncrementoDecremento.java
@@ -0,0 +1,16 @@
+public class IncrementoDecremento {
+    public static void main(String[] args) {
+        int numero = 9;
+        System.out.println(numero);
+
+        numero++;
+        System.out.println(numero);
+        
+        numero--;
+        System.out.println(numero);
+
+        System.out.println(++numero);
+
+        System.out.println(--numero);
+    }
+}
