c7d352c773c8258518e2dd04ce21ca6d82287181
Victor Lima Bezerra <lima.victoradm@gmail.com>
2024-06-25 20:02:32 -0300

    adição de mais exemplos de construtores

---
+++ execicio-construtor/src/Humano.java
@@ -0,0 +1,10 @@
+public class Humano {
+ public static void main(String[] args) {
+    Pessoa wesley = new Pessoa("Side-cut", 24);
+    Pessoa victor = new Pessoa("careca", 24);
+
+
+    System.out.println("Wesley: "+wesley);
+    System.out.println("Victor: "+victor);
+ }
+}
