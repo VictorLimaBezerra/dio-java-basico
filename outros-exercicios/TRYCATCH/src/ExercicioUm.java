de2e2e5e7f7703cb61f4597b891ed55a9004c4d4
Victor Lima Bezerra <lima.victoradm@gmail.com>
2024-06-27 10:35:39 -0300

    adição de exercicio sobre exception

---
+++ outros-exercicios/TRYCATCH/src/ExercicioUm.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Locale;
+
+public class ExercicioUm {
+    public static void main(String[] args) {
+
+        try{
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o teu nome.");
+        String nome = scanner.next();
+
+        System.out.println("Digite a última parte do teu sobrenome.");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a tua idade.");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a tua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Nome fornecido: " + nome.toUpperCase());
+        System.out.println("Sobrenome fornecido: " + sobrenome.toUpperCase());
+        System.out.println("Idade fornecida: " + idade);
+        System.out.println("Altura fornecida: " + altura);
+        scanner.close();
+
+        }catch(InputMismatchException e){
+            System.out.println("Os campos altura e idade são NÚMERICOS");
+        }
+
+    }
+}
